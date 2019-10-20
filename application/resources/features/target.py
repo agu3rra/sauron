fbdeba72e638dd82ffd1f4b477ce3f287db2aab4
Andre <agu3rra@gmail.com>
2019-10-20 06:09:30 -0300

    removed target.py

--- application/resources/features/target.py
+++
@@ -1,9 +0,0 @@
-class Target():
-
-    def __init__(self, host, port, guid=None, name=None):
-        self.host = host
-        self.port = port
-        self.guid = guid
-        self.name = name
-
-        
