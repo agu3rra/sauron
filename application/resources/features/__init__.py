532bbff03400d6b1e23b2476c978fcb1932a5472
Andre <agu3rra@gmail.com>
2019-10-27 16:24:44 -0300

    progressed on scan endpoint

---
+++ application/resources/features/__init__.py
@@ -0,0 +1,1 @@
+from .encryption_check import EncryptionCheck
--- application/resources/scan.py
+++ application/resources/scan.py
@@ -1,12 +1,38 @@
 import json
-from flask import Blueprint, Response
+from flask import Blueprint, Response, request
 from .scan_templates import scan_definitions, result_template
+from . import features as ft
 
 
 scan = Blueprint('scan', __name__)
 
-@scan.route('/scan')
+@scan.route('/scan', methods=['post'])
 def trigger_scan():
+    data = request.get_json()
+    
+    # Validate scan input
+    def validate_scan_input(data):
+        if "target_host" in data and "target_port" in data:
+            return True
+        return False        
+    if not validate_scan_input(data):
+        return Response(json.dumps({"response":False,
+                                    "info":"This endpoint requires "\
+                                        "\"target_host\" and \"target_port\""}),
+                    status=400,
+                    mimetype='application/json')
+    
+    proxy_settings = data.get('proxy', None)
+    check = ft.EncryptionCheck(host=data['target_host'], port=data['target_port'], proxy=proxy_settings)
+    results = check.scan()
+
+    if len(results) == 0:
+        # service did not respond.
+        pass
+
+    # Trigger encryption check
     return Response(json.dumps({"status":"not yet implemented"}),
                     status=500,
                     mimetype='application/json')
+
+    
