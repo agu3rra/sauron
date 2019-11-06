a714c0bd6ab88ede042d2e5dbafd8229c774f172
Andre <agu3rra@gmail.com>
2019-11-06 06:25:17 -0300

    added simple integration tests for status and scan

--- application/tests/test_integration.py
+++ application/tests/test_integration.py
@@ -15,4 +15,20 @@ class TestIntegration():
             yield client
 
     def test_status(self, client):
-        pass
+        response = client.get('/')
+        response = response.json
+        assert isinstance(response, dict)
+        assert response['result'] == True
+        assert response['info'].find('Sauron') != -1
+
+    def test_scan(self, client):
+        scan_data = {
+            "target_host":"uol.com",
+            "target_port":443
+        }
+        response = client.post('/scan', json=scan_data)
+        response = response.json
+        assert isinstance(response, dict)
+        assert response['result'] == False
+        num_encryption_checks = len(response['results'][0]['checks'])
+        assert num_encryption_checks == 6
